19b7808ad071e3d95eb5dd3561d6ecd1f36949bb
i1876 <i1876@naver.com>
2013-12-18 17:34:51 +0900

    fdsfdsfdsfds

---
+++ aaa/src/com/example/aaa/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.aaa;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+public class MainActivity extends Activity {
+
+	//String url = "http://192.168.0.22:3000/index.html";
+	String url = "file:///android_asset/index.html";
+	WebView webViewMain;
+	@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webViewMain = (WebView) findViewById(R.id.web1);
+        WebSettings webSettings = webViewMain.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setBuiltInZoomControls(true);
+        webViewMain.loadUrl(url);
+        
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
+}
